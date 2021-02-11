14c3002d7e2c62d026c8d12c7619900302e35837
Hoshi <hoshimure.47@gmail.com>
2021-02-12 00:52:09 +0900

    :+1: add @google/model-viewer

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -5,3 +5,23 @@ declare namespace NodeJS {
     readonly NODE_ENV: "development" | "production" | "test";
   }
 }
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    "model-viewer": ModelViewerJSX;
+  }
+}
+
+interface ModelViewerJSX {
+  src: string;
+  alt?: string;
+  "camera-controls"?: boolean;
+  autoplay?: boolean;
+  style?: CSSProperties;
+}
+
+interface ModelViewerElement extends Element {
+  currentTime: number;
+  play(): void;
+  pause(): void;
+}
